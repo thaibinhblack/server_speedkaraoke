0b3a072b7e18d37b99daf61f7a306d33c6ade68f
thaibinhblack <thaibinhblack@gmail.com>
2019-11-16 16:59:00 +0700

    add field model like

--- .history/app/model/RatingLikeModel_20191116165831.php
+++ .history/app/model/RatingLikeModel_20191116165831.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class RatingLikeModel extends Model
 {
     protected $table = "tale_rating_like";
-    protected $fillable = ["UUID_RATING_LIKE", "UUID_USER", "UUID_BAR_KARAOKE", "TYPE"];
+    protected $fillable = ["UUID_RATING_LIKE", "UUID_USER", "UUID_BAR_KARAOKE", "UUID_ROOM_BAR_KARAOKE", "TYPE", "NUMBER_LIKE"];
 }
